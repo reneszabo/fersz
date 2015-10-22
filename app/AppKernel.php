4ec948ea05594d49ffea147124e872076a4d0d16
Rene Ramirez <rene.ramirez@fersz.com>
2015-10-21 20:59:35 -0400

    Add channels

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel {
         new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         new JMS\SerializerBundle\JMSSerializerBundle(),
         new FrontendBundle\FrontendBundle(),
+        new Sonata\SeoBundle\SonataSeoBundle(),
     );
 
     if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/FrontendBundle/Controller/DefaultController.php
+++ src/FrontendBundle/Controller/DefaultController.php
@@ -11,11 +11,28 @@ class DefaultController extends Controller {
   }
 
   public function imageAction($imageName = "") {
+
+    $this->getSeoPage()
+            ->setTitle($imageName)
+            ->addMeta('property', 'og:description', "Logo of our client - " . $imageName)
+            ->addMeta('property', 'og:type', 'image')
+    ;
+
     return $this->render('FrontendBundle:Page:image.html.twig', array('imageName' => $imageName));
   }
 
   public function sitemapAction() {
     return $this->render('FrontendBundle:Sitemap:sitemap.xml.twig', array());
+  }
+  public function canalesAction() {
+    return $this->render('FrontendBundle:Canales:canales.m3u.twig', array());
+  }
+
+  /**
+   * @return \Sonata\SeoBundle\Seo\SeoPageInterface
+   */
+  public function getSeoPage() {
+    return $this->get('sonata.seo.page');
   }
 
 }
